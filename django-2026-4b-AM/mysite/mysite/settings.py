261dc4952d7bc1af63f74e61f73305ed35d6aaf2
moradr9700 <moradr9700@gmail.com>
2025-11-26 08:54:41 +0100

    Add files via upload

--- django-2026-4b-AM/mysite/mysite/settings.py
+++ django-2026-4b-AM/mysite/mysite/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['prnt2026.pythonanywhere.com', '127.0.0.1', 'localhost']
